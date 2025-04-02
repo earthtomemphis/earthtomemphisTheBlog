41541449a2273b47bb1090ba7337dfa019706c26
Memphis DiAngelis <earthtomemphis@gmail.com>
2025-04-02 11:59:45 -0600

    create basic express server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,22 @@
-function sayHello(name: string) {
-	console.log('Hello' + name);
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+async function start() {
+	dotenv.config();
+	const port = process.env.PORT || 3000;
+
+	const app: Express = express();
+
+	app.use(express.urlencoded({ extended: true }));
+	app.use(express.json());
+
+	app.get('/', (req: Request, res: Response) => {
+		res.send('Hello World');
+	});
+
+	app.listen(port, () => {
+		console.log(`Server is running at http://localhost:${port}`);
+	});
 }
 
-sayHello('world!');
+start();
