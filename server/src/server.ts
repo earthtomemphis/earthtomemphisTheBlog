0790539714cff5b699dce83fcdcc8e2d7c60d1ef
Memphis DiAngelis <earthtomemphis@gmail.com>
2025-05-22 22:44:46 -0600

    created post index and show routes with links

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import { v4 as uuid } from 'uuid';
 
 async function start() {
 	dotenv.config();
@@ -13,8 +14,36 @@ async function start() {
 	app.set('view engine', 'ejs');
 	app.set('views', path.join(__dirname, '/views'));
 
+	const posts = [
+		{
+			id: uuid(),
+			title: 'Post 1',
+			body: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium corporis fuga sunt. Explicabo tempore saepe beatae nulla non necessitatibus molestiae obcaecati, optio quis aspernatur exercitationem quos assumenda, incidunt ut soluta!',
+		},
+		{
+			id: uuid(),
+			title: 'Post 2',
+			body: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium corporis fuga sunt. Explicabo tempore saepe beatae nulla non necessitatibus molestiae obcaecati, optio quis aspernatur exercitationem quos assumenda, incidunt ut soluta!',
+		},
+		{
+			id: uuid(),
+			title: 'Post 3',
+			body: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium corporis fuga sunt. Explicabo tempore saepe beatae nulla non necessitatibus molestiae obcaecati, optio quis aspernatur exercitationem quos assumenda, incidunt ut soluta!',
+		},
+	];
+
 	app.get('/', (req: Request, res: Response) => {
-		res.render('home');
+		res.render('home', { posts });
+	});
+
+	app.get('/posts', (req: Request, res: Response) => {
+		res.render('posts/index', { posts });
+	});
+
+	app.get('/posts/:id', (req: Request, res: Response) => {
+		const { id } = req.params;
+		const post = posts.find((p) => p.id === id);
+		res.render('posts/show', { post });
 	});
 
 	app.listen(port, () => {
