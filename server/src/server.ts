4acccebbbdde3e9974ad2112c427b47052b490e9
Memphis DiAngelis <earthtomemphis@gmail.com>
2025-07-20 17:23:11 +0700

    added mongo and full posts crud

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,8 @@ import { v4 as uuid } from 'uuid';
 import mongoose from 'mongoose';
 // @ts-ignore
 import ejsMate from 'ejs-mate';
+import methodOverride from 'method-override';
+import Post from './models/post';
 
 async function start() {
 	dotenv.config();
@@ -17,14 +19,15 @@ async function start() {
 	app.set('views', path.join(__dirname, '/views'));
 	app.use(express.urlencoded({ extended: true }));
 	app.use(express.json());
+	app.use(methodOverride('_method'));
 
-	// mongoose.connect('mongodb://127.0.0.1:27017/earthtomemphis');
+	mongoose.connect('mongodb://127.0.0.1:27017/earthtomemphis');
 
-	// const db = mongoose.connection;
-	// db.on('error', console.error.bind(console, 'connection error:'));
-	// db.once('open', () => {
-	// 	console.log('Database connected');
-	// });
+	const db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', () => {
+		console.log('Database connected');
+	});
 
 	const posts = [
 		{
@@ -48,24 +51,41 @@ async function start() {
 		res.render('home');
 	});
 
+	app.get('/posts', async (req: Request, res: Response) => {
+		const posts = await Post.find({});
+		res.render('posts/index', { posts });
+	});
+
 	app.get('/posts/new', (req: Request, res: Response) => {
 		res.render('posts/new');
 	});
 
-	app.post('/posts', (req: Request, res: Response) => {
-		const { title, body } = req.body;
-		posts.push({ title, body, id: uuid() });
-		res.redirect('/posts');
+	app.post('/posts', async (req: Request, res: Response) => {
+		const post = new Post(req.body.post);
+		await post.save();
+		res.redirect(`/posts/${post._id}`);
 	});
 
-	app.get('/posts', (req: Request, res: Response) => {
-		res.render('posts/index', { posts });
-	});
-
-	app.get('/posts/:id', (req: Request, res: Response) => {
-		const { id } = req.params;
-		const post = posts.find((p) => p.id === id);
+	app.get('/posts/:id', async (req: Request, res: Response) => {
+		const post = await Post.findById(req.params.id);
 		res.render('posts/show', { post });
+	});
+
+	app.get('/posts/:id/edit', async (req: Request, res: Response) => {
+		const post = await Post.findById(req.params.id);
+		res.render('posts/edit', { post });
+	});
+
+	app.patch('/posts/:id', async (req: Request, res: Response) => {
+		const { id } = req.params;
+		const post = await Post.findByIdAndUpdate(id, { ...req.body.post });
+		res.redirect(`/posts/${post?._id}`);
+	});
+
+	app.delete('/posts/:id', async (req: Request, res: Response) => {
+		const { id } = req.params;
+		const post = await Post.findByIdAndDelete(id);
+		res.redirect('/posts');
 	});
 
 	app.listen(port, () => {
