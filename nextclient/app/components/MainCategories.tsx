c0b258417d0ec1e337aea78a5c3582bb20950885
Memphis DiAngelis <earthtomemphis@gmail.com>
2025-12-11 16:21:22 +0700

    updated search component

--- nextclient/app/components/MainCategories.tsx
+++ nextclient/app/components/MainCategories.tsx
@@ -1,5 +1,5 @@
-import { Input } from '@/components/ui/input';
 import Link from 'next/link';
+import Search from './Search';
 
 const MainCategories = () => {
 	return (
@@ -39,7 +39,8 @@ const MainCategories = () => {
 			</div>
 			<span className="text-xl font-medium">|</span>
 			{/* search */}
-			<div className="bg-gray-100 p-2 rounded-full flex items-center gap-2">
+			<Search />
+			{/* <div className="bg-gray-100 p-2 rounded-full flex items-center gap-2">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 24 24"
@@ -51,12 +52,12 @@ const MainCategories = () => {
 					<circle cx="10.5" cy="10.5" r="7.5" />
 					<line x1="16.5" y1="16.5" x2="22" y2="22" />
 				</svg>
-				<Input
+				<input
 					type="text"
 					placeholder="search a post..."
 					className="bg-transparent border-none h-max w-max"
 				/>
-			</div>
+			</div> */}
 		</div>
 	);
 };
